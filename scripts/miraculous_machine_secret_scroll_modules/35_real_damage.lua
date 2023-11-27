3e193a3a33d262fc18645a93289724d5103aeb33
UnderDarkNight <under.dark.night@outlook.com>
2023-11-27 11:42:14 +0800

    real damage sys bug fix

--- scripts/miraculous_machine_secret_scroll_modules/35_real_damage.lua
+++ scripts/miraculous_machine_secret_scroll_modules/35_real_damage.lua
@@ -5,7 +5,7 @@
 
     · 击杀【远古犀牛】  minotaur
         · 击杀1只犀牛后解锁【真实伤害】，攻击造成额外10点真实伤害（可被二连击触发）
-        · 每击杀1只犀牛增加2点，最高20点真伤 .   5只 max
+        · 每击杀1只犀牛增加2点，最高20点真伤 .   6只 max
 ]]--
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
 return {
@@ -18,8 +18,8 @@ return {
                 local minotaur_num = inst.components.miraculous_machine_secret_scroll:Get("boss.kill.minotaur")
                 if minotaur_num then
                     minotaur_num = minotaur_num or 0
-                    if minotaur_num > 5 then
-                        minotaur_num = 5
+                    if minotaur_num > 6 then
+                        minotaur_num = 6
                     end
                     local damage = -1*( 10 + (minotaur_num - 1)*2 )
                     _table.target.components.health:DoDelta(damage)
